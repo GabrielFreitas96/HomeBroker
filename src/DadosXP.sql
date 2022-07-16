f80355c0ba3aea6ac41a1a822fdd6dc55feb4ba4
GabrielFreitas96 <gabrielbfreitas96@gmail.com>
2022-07-16 16:20:20 -0300

    feat: Query para criação do banco de dados

---
+++ src/DadosXP.sql
@@ -0,0 +1,62 @@
+ DROP DATABASE IF EXISTS DadosXp;
+
+CREATE DATABASE DadosXp;
+CREATE TABLE DadosXp.Contas(
+codConta BIGINT NOT NULL PRIMARY KEY,
+saldo DECIMAL(12,2) NOT NULL
+) engine = InnoDB;
+
+INSERT INTO DadosXp.Contas (codConta, saldo)
+VALUES
+(12345, 10000.00),
+(23456,	20000.00),
+(34567,	30000.00);
+
+
+
+CREATE TABLE DadosXp.Clientes(
+codCliente INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+nameCliente  VARCHAR(100) NOT NULL,
+emailCliente VARCHAR(30) NOT NULL,
+passwordCliente VARCHAR(15) NOT NULL,
+contaCliente BIGINT NOT NULL,
+FOREIGN KEY (contaCliente) REFERENCES DadosXp.Contas(codConta)
+) engine = InnoDB;
+INSERT INTO DadosXp.Clientes(nameCliente, emailCliente, passwordCliente, contaCliente)
+VALUES
+( 'Gabriel Freitas', 'gabrielfreitas@gmail.com', '123456', 12345),
+('José Almeida', 'josealmeida@gmail.com', 'abcdef', 23456),
+('Lucas Ferreira','lucasferreira@gmail.com','a1b2c3d4', 34567);
+
+CREATE TABLE DadosXp.Ativos (
+codAtivo INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+nameAtivo VARCHAR(15) NOT NULL,
+qtdeAtivo INT NOT NULL,
+valor DECIMAL(10,2)
+ ) engine = InnoDB;
+ INSERT INTO DadosXp.Ativos( nameAtivo, qtdeAtivo, valor)
+ VALUES
+ ('QUAT', 1000, 10.00),
+ ('Gol', 2000, 20.00),
+ ('Armac', 3000, 30.00),
+ ('Azul', 4000, 40.00);
+ 
+ CREATE TABLE DadosXp.ClientesAtivos (
+ codCliente INT NOT NULL,
+ codAtivo INT NOT NULL,
+ qtdeClienteAtivo INT NOT NULL,
+ FOREIGN KEY (codCliente) REFERENCES DadosXp.Clientes(codCliente),
+ FOREIGN KEY (codAtivo) REFERENCES DadosXp.Ativos(codAtivo),
+ CONSTRAINT PRIMARY KEY (codCliente, codAtivo)
+ ) engine = InnoDB;
+ 
+ INSERT INTO DadosXp.ClientesAtivos (codCliente, codAtivo, qtdeClienteAtivo)
+ VALUES
+ (1, 1, 10),
+ (1, 2, 20),
+ (1, 3, 5),
+ (1, 4, 2),
+ (2, 1, 10),
+ (2, 2, 5),
+ (3, 4, 10),
+ (3, 3, 5);
