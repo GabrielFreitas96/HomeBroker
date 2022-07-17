439753aea85d60fc703194cf6000c76a11a2b1fd
GabrielFreitas96 <gabrielbfreitas96@gmail.com>
2022-07-17 14:53:40 -0300

    feat: service para Cliente, regras para adicionar um cliente

---
+++ src/services/clienteService.ts
@@ -0,0 +1,30 @@
+import ICliente from '../interfaces/ICliente';
+import IObjResponse from '../interfaces/IObjResponse';
+import clienteModel from '../models/clienteModel';
+import contaModel from '../models/contaModel';
+import { generateCryptPassword } from '../utils/bcrypt';
+import ObjCode from '../utils/ObjCodes';
+
+const addCliente = async (newcliente: ICliente):Promise<IObjResponse> => {
+  const { affectedRows } = await contaModel.addConta(+newcliente.contaCliente);
+  if (affectedRows !== 1) {
+    const response:IObjResponse = { status: ObjCode.GENERAL, message: 'Unexpected Error' };
+    return response;
+  }
+  const passwordHash = generateCryptPassword(newcliente.passwordCliente);
+  const clienteBD = { ...newcliente, clientePassword: passwordHash };
+  console.log('clienteBD', clienteBD);
+  const { insertId } = await clienteModel.addUser(clienteBD);
+  if (!insertId) {
+    const response:IObjResponse = { status: ObjCode.GENERAL, message: 'Unexpected Error' };
+    return response;
+  }
+  const { nameCliente, emailCliente, contaCliente } = clienteBD;
+  const payload = [{
+    codCliente: insertId, nameCliente, emailCliente, contaCliente,
+  }];
+  const response:IObjResponse = { status: ObjCode.CREATED, payload };
+  return response;
+};
+const clienteService = { addCliente };
+export default clienteService;
