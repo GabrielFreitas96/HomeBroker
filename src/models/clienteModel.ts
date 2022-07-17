27de53037a6f54777556f000ca962a30b6377346
GabrielFreitas96 <gabrielbfreitas96@gmail.com>
2022-07-17 14:52:57 -0300

    feat: adiciona o Cliente na tabela Clientes

--- src/models/clienteModel.ts
+++ src/models/clienteModel.ts
@@ -1,3 +1,4 @@
+import { ResultSetHeader } from 'mysql2';
 import ICliente from '../interfaces/ICliente';
 import connection from './connection';
 
@@ -7,5 +8,15 @@ const getClient = async (contaCliente:number): Promise<ICliente[]> => {
   console.log('result', result);
   return result as ICliente[];
 };
-const clienteModel = { getClient };
+
+const addUser = async (cliente: ICliente):Promise<ResultSetHeader> => {
+  const query = 'INSERT INTO DadosXp.Clientes (nameCliente, emailCliente, passwordCliente, contaCliente) VALUE (?, ?, ?, ?)';
+  const {
+    nameCliente, emailCliente, passwordCliente, contaCliente,
+  } = cliente;
+  const [result] = await connection
+    .execute<ResultSetHeader>(query, [nameCliente, emailCliente, passwordCliente, contaCliente]);
+  return result;
+};
+const clienteModel = { getClient, addUser };
 export default clienteModel;
