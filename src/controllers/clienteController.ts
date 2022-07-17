348d2d44dff1435aba5147fb73b4afa11e98098d
GabrielFreitas96 <gabrielbfreitas96@gmail.com>
2022-07-17 14:54:19 -0300

    feat: Controller para o cliente

--- src/controllers/clienteController.ts
+++ src/controllers/clienteController.ts
@@ -1,7 +1,17 @@
 import { Request, Response } from 'express';
+import ICliente from '../interfaces/ICliente';
+import clienteService from '../services/clienteService';
 
-const addNew = async (req: Request, res: Response) => {
-  res.status(200).json({ message: 'Clientes' });
+const addNew = async (req: Request, res: Response): Promise<Response> => {
+  const newCliente: ICliente = req.body;
+  console.log('newCliente', newCliente);
+  const response = await clienteService.addCliente(newCliente);
+  // console.log('response', response);
+  if (response.message) {
+    const { message } = response;
+    return res.status(response.status).json({ message });
+  }
+  return res.status(response.status).json(response.payload);
 };
 
 const clienteController = { addNew };
