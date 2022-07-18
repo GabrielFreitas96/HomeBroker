971ea3c52a69190ff8d94f6ded020fa9c716e27b
GabrielFreitas96 <gabrielbfreitas96@gmail.com>
2022-07-18 12:54:02 -0300

    feat:Criação do ser service para deposito

--- src/services/contaService.ts
+++ src/services/contaService.ts
@@ -13,5 +13,26 @@ const getByCodCliente = async (id: number):Promise<IObjResponse> => {
   return response as IObjResponse;
 };
 
-const contaService = { getByCodCliente };
+const contaDeposito = async (codCliente: number, valor:number):Promise<IObjResponse> => {
+  const cliente = await contaModel.getByCodCliente(codCliente);
+  // console.log(cliente);
+  if (cliente.length === 0) {
+    const response:IObjResponse = { status: ObjCode.NOT_FOUND, message: `O id ${codCliente} was not found` };
+    return response;
+  }
+  const conta = await contaModel.getContaByCodCliente(codCliente);
+  // console.log('conta saldo', conta[0].saldo);
+  const newSaldo = Math.round((+conta[0].saldo + +valor) * 100) / 100;
+  // console.log(newSaldo);
+  const { affectedRows } = await contaModel.updateSaldo(conta[0].codConta, newSaldo);
+  // console.log('afect', affectedRows);
+  if (affectedRows !== 1) {
+    const response:IObjResponse = { status: ObjCode.GENERAL, message: 'Unexpected Error' };
+    return response;
+  }
+  const response = { status: ObjCode.OK, payload: [{ codCliente, saldo: newSaldo }] };
+  return response as IObjResponse;
+};
+
+const contaService = { getByCodCliente, contaDeposito };
 export default contaService;
