39412cd3b7654b65407aefd844d4195b93ee5150
GabrielFreitas96 <gabrielbfreitas96@gmail.com>
2022-07-18 12:55:27 -0300

    feat: adição de rota /conta/deposito PUT, para deposito em conta

--- src/routers/contaRouter.ts
+++ src/routers/contaRouter.ts
@@ -2,5 +2,6 @@ import { Router } from 'express';
 import contaController from '../controllers/contaController';
 
 const contaRouter = Router();
+contaRouter.put('/deposito', contaController.contaDeposito);
 contaRouter.get('/:id', contaController.getByCodCliente);
 export default contaRouter;
