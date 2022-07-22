2ace7a9f1cdee2c168f9207e3594a1d843ee0751
GabrielFreitas96 <gabrielbfreitas96@gmail.com>
2022-07-22 17:38:32 -0300

    feat: swagger para loginRouter

--- src/routers/loginRouter.ts
+++ src/routers/loginRouter.ts
@@ -6,5 +6,53 @@ import verifyContaLogin from '../middlewares/verifyContaLogin';
 const loginRouter = Router();
 
 loginRouter.post('/', contaPasswordLogin, verifyContaLogin, loginController.makeLogin);
-
+/** 
+ * @swagger
+ *  tags:
+ *    name: Login
+ *    description: Realiza o login do Cliente, gerando o Token
+*/
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Login:
+ *       type : object
+ *       required:
+ *         - contaCliente
+ *         - passwordCliente
+ *       properties:
+ *         codCliente:
+ *           type: number
+ *         passwordCliente:
+ *           type: string
+ *       example:
+ *         contaCliente: 12345
+ *         passwordCliente: '123456'
+ */
+/**
+ * @swagger
+ *  /login:
+ *    post:
+ *      tags: [Login]
+ *      description: Realiza a venda de ativos de um cliente
+ *      security:
+ *        - bearerAuth: []  
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              $ref: '#components/schemas/Login'
+ *      responses:
+ *        200:
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  token:
+ *                    type: string
+ */
 export default loginRouter;
