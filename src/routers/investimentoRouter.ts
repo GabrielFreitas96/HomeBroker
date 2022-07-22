e170112463db70bab8de30d62bad3a433d43b379
GabrielFreitas96 <gabrielbfreitas96@gmail.com>
2022-07-22 17:23:53 -0300

    feat: swagger para rota investimento

--- src/routers/investimentoRouter.ts
+++ src/routers/investimentoRouter.ts
@@ -7,5 +7,77 @@ import verifyCodClienteCodAtivo from '../middlewares/verifyCodClienteCodAtivo';
 const investimentoRouter = Router();
 
 investimentoRouter.put('/vender', authenticationSaqueDeposito, investimentoComprarVender, investimentoController.sellAtivos);
+/** 
+ * @swagger
+ *  tags:
+ *    name: Investimento
+ *    description: Realiza operações de imvestimento, compra e venda
+*/
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Investimento:
+ *       type : object
+ *       required:
+ *         - codCliente
+ *         - codAtivo
+ *         - qtdeAtivo
+ *       properties:
+ *         codCliente:
+ *           type: number
+ *         codAtivo:
+ *           type: number
+ *         qtdeAtivo:
+ *           type: number
+ *       example:
+ *         codCliente: 1
+ *         codAtivo: 3
+ *         saldo : 2
+ */
+/**
+ * @swagger
+ *  /investimento/vender:
+ *    put:
+ *      tags: [Investimento]
+ *      description: Realiza a venda de ativos de um cliente
+ *      security:
+ *        - bearerAuth: []  
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              $ref: '#components/schemas/Investimento'
+ *      responses:
+ *        200:
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: array 
+ */
 investimentoRouter.post('/comprar', authenticationSaqueDeposito, investimentoComprarVender, verifyCodClienteCodAtivo, investimentoController.buyAtivos);
 export default investimentoRouter;
+/**
+ * @swagger
+ *  /investimento/comprar:
+ *    put:
+ *      tags: [Investimento]
+ *      description: Realiza a compra de ativos de um cliente
+ *      security:
+ *        - bearerAuth: []  
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              $ref: '#components/schemas/Investimento'
+ *      responses:
+ *        200:
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: array 
+ */
