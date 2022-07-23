6e9aa99dbb7dd1c29fbb372675cab27da3dec423
GabrielFreitas96 <gabrielbfreitas96@gmail.com>
2022-07-23 19:47:53 -0300

    style: retirando os console.log

--- src/middlewares/authenticationConta.ts
+++ src/middlewares/authenticationConta.ts
@@ -9,24 +9,13 @@ dotenv.config();
 const authenticationConta = (req: Request, res: Response, next: NextFunction) => {
   const auth= req.headers.authorization;
   const { id } = req.params;
-  // console.log(auth);
-  //console.log('resultado do split', token);
-  // console.log('id', typeof id);
-  // console.log('token no middleware verify', token);
   if (!auth) {
     return res.status(ObjCode.UNAUTHORIZED).json({ message: 'Token not found' });
   }
   const token = splitString(auth);
   try {
-    // const decoded = jwt.verify(token, secret);
-    // console.log('decoded no middleware', decoded);
     const decoded = decodeToken(token);
-    // const { codCliente } = decoded;
-    // console.log('adsd', typeof codCliente);
-    // console.log('decoded', decoded);
-    // console.log(decoded.payload.codCliente);
     if (decoded.codCliente !== +id) {
-      // console.log('entrou no if');
       return res.status(ObjCode.UNAUTHORIZED).json({ message: 'Unauthorized client' });
     }
   } catch (error) {
