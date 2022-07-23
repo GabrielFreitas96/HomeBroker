4eab6b9d7abb482e2253135856403fedc2327beb
GabrielFreitas96 <gabrielbfreitas96@gmail.com>
2022-07-23 18:48:35 -0300

    feat: Criando arquivos com mocks

---
+++ src/test/mockAtivos.ts
@@ -0,0 +1,30 @@
+import IAtivos from '../interfaces/IAtivo';
+
+const Ativos : IAtivos[] = [
+  {
+    codAtivo: 1,
+    nameAtivo: "QUAT",
+    qtdeAtivo: 1000,
+    valor: 10.00
+  },
+  {
+    codAtivo: 2,
+    nameAtivo: "Gol",
+    qtdeAtivo: 2000,
+    valor: 20.00
+  },
+  {
+    codAtivo: 3,
+    nameAtivo: "Armac",
+    qtdeAtivo: 3000,
+    valor: 30.00
+  },
+  {
+    codAtivo: 4,
+    nameAtivo: "Azul",
+    qtdeAtivo: 4000,
+    valor: 40.00
+  }
+];
+
+export default Ativos;
---
+++ src/test/mockClienteAtivo.ts
@@ -0,0 +1,33 @@
+
+const ClienteAtivos : any [] = [
+	{
+		codCliente: 1,
+		codAtivo: 1,
+		nameAtivo: 'QUAT',
+		qtdeClienteAtivo: 10,
+		valor: 10.00
+	},
+	{
+		codCliente: 1,
+		codAtivo: 2,
+		nameAtivo: 'Gol',
+		qtdeClienteAtivo: 20,
+		valor: 20.00
+	},
+	{
+		codCliente: 1,
+		codAtivo: 3,
+		nameAtivo: 'Armac',
+		qtdeClienteAtivo: 3,
+		valor: 30.00
+	},
+	{
+		codCliente: 1,
+		codAtivo: 4,
+		nameAtivo: 'Azul',
+		qtdeClienteAtivo: 2,
+		valor: 40.00
+	}
+];
+
+export default ClienteAtivos;
